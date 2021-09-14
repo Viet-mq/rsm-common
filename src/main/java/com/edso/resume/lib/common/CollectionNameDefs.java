cfb7c15ea2e6e45f0197f4df04f8e640ea340765
NoInfo <nonamenoface20190504@gmail.com>
2021-09-14 14:34:10 +0700

    Add exception define

--- src/main/java/com/edso/resume/lib/common/CollectionNameDefs.java
+++ src/main/java/com/edso/resume/lib/common/CollectionNameDefs.java
@@ -1,4 +1,16 @@
 package com.edso.resume.lib.common;
 
 public class CollectionNameDefs {
+    public static final String COLL_USER = "coll_user";
+    public static final String COLL_PERMISSION = "coll_permission";
+    public static final String COLL_FRONTEND_VIEW = "coll_frontend_view";
+    public static final String COLL_FRONTEND_VIEW_ACTION = "coll_frontend_view_action";
+    public static final String COLL_FRONTEND_MENU = "coll_frontend_menu";
+    public static final String COLL_FRONTEND_MENU_ITEMS = "coll_frontend_menu_items";
+    public static final String COLL_FRONTEND_MENU_USER = "coll_frontend_menu_user";
+
+    public static final String COLL_API_ROLE = "coll_api_role";
+    public static final String COLL_API_ROLE_GROUP = "coll_api_role_group";
+    public static final String COLL_API_ROLE_GROUP_ITEMS = "coll_api_role_group_items";
+    public static final String COLL_API_ROLE_GROUP_USER = "coll_api_role_group_user";
 }
