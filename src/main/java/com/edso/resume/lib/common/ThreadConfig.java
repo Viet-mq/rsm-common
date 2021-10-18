961d047facf3cb759eb041fe3096f36fc367a923
Quan Pham <quanpk@edsolabs.com>
2021-10-18 08:41:46 +0700

    Update: - Add smt in CollectionNameDefs Add: - GetArrayCalendarReponse

---
+++ src/main/java/com/edso/resume/lib/common/ThreadConfig.java
@@ -0,0 +1,12 @@
+package com.edso.resume.lib.common;
+
+public class ThreadConfig {
+    public static final String JOB = "job";
+    public static final String JOB_LEVEL = "jobLevel";
+    public static final String SCHOOL = "school";
+    public static final String SOURCE_CV = "sourceCV";
+    public static final String PROFILE = "profile";
+    public static final String STATUS_CV = "statusCV";
+    public static final String DEPARTMENT = "department";
+    public static final String CALENDAR = "calendar";
+}
---
+++ src/main/java/com/edso/resume/lib/common/TypeConfig.java
@@ -0,0 +1,8 @@
+package com.edso.resume.lib.common;
+
+public class TypeConfig {
+    public static final String SELECT = "select";
+    public static final String CREATE = "create";
+    public static final String UPDATE = "update";
+    public static final String DELETE = "delete";
+}
