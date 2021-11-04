5e9553838bec2e5c1aa331bebf8971294b33bf55
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-04 14:36:05 +0700

    update: RabbitMQConfig

---
+++ src/main/java/com/edso/resume/lib/common/RabbitMQConfig.java
@@ -0,0 +1,9 @@
+package com.edso.resume.lib.common;
+
+public class RabbitMQConfig {
+    public static final String UPDATE = "update";
+    public static final String CREATE = "create";
+    public static final String DELETE = "delete";
+    public static final String UPDATE_DETAIL = "update-detail";
+    public static final String UPDATE_STATUS = "update-status";
+}
